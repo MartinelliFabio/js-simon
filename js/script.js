57e9c8fb27a2eb31b0f95bf4e78c583064150614
MartinelliFabio <fabio.martinelli996@gmail.com>
2022-10-20 10:40:03 +0200

    reordered the code in the script.js + add comments

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 "use strict";
 /*  -----------------------------------------------------------------------------------------------
-    Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
-    Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite una casella di input e un bottone
+    Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 3 secondi.
+    Dopo 3 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite una casella di input e un bottone
     Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 --------------------------------------------------------------------------------------------------- */
+
+//Sezione delle function()
 
 // Creo l'array per raccogliere i numeri randomici
 let numeri = [];
@@ -24,7 +26,7 @@ function createNumbers () {
     const numeriHTML = document.getElementById('numeri');
     numeriHTML.innerHTML = `Questi sono i 5 numeri generati casualemente: <br> ${numeri}`;
 }
-createNumbers(); // Richiamo la funzione
+
 
 // Creo la variabile timer assegnando un intervallo di 3 secondi
 const timer = setInterval(timerNumeri, 3000);
@@ -34,14 +36,15 @@ function timerNumeri () {
     document.getElementById("numeri").classList.add('invisible');
 }
 
+// Dopo i 3 secondi del timer e che i numeri randomici sono scomparsi, chiedi all'utente di inserire e indovinare i 5 numeri precedentemente stampatia schermo
 const pushaInputHTML = document.getElementById('pusha-input');
-let arrInputUtente = [];
+let arrInputUtente = []; // Creo un array vuoto dove inserirò i 5 numeri inseriti
 const outputNumeriHTML = document.getElementById('output-numeri');
 
 function pushaNumero() {
-
     let inputNumber = parseInt(document.getElementById('input').value);
     
+    // Operatore condizionale if per vedere se l'utente inserisce un numero e/o il limite di 5 numeri da inserire
     if(input.value == "") {
         alert('Devi Scrivere qualcosa');
     } else if(arrInputUtente.length < numeriDiNumeri) {
@@ -51,30 +54,29 @@ function pushaNumero() {
     }
     input.value = '';
 }
-pushaInputHTML.addEventListener('click', () => {
-    pushaNumero();
-})
 
+// Ora verifico se i numeri inseriti dall'utente siano uguali a quelli generati randomicamente
 const verificaInputHTML = document.getElementById('verifica-input');
 const risultatoHTML = document.getElementById('risultato');
-let contatore = 0;
+let contatore = 0; // contatore per vedere quanti elementi ha mancato l'utente
 
 function verificaNumeri() {
+    const numeriGiusti = []; // array per racchiudere i numeri indovinati dall'utente
     for(let i = 0; i < numeriDiNumeri; i++) {
+        // Operatore condizionale if per vedere se i numeri inseriti dall'utente siano uguali a quelli randomici
         if(arrInputUtente.includes(numeri[i])) {
-            risultatoHTML.innerHTML = 'Hai indovinato tutti i numeri!';
+            numeriGiusti.push(arrInputUtente[i]);
+            risultatoHTML.innerHTML = 'Hai indovinato tutti i numeri! <br> I numeri indovinati sono: ' + numeri;
         } else {
             contatore++;
             console.log('no');
-            risultatoHTML.innerHTML = 'Hai mancato: ' + contatore + ' numeri!';
+            risultatoHTML.innerHTML = 'Hai mancato: ' + contatore + ' numeri! <br> I numeri indovinati sono: ' + numeriGiusti;
         }
     }
+    console.log(numeriGiusti);
 }
-verificaInputHTML.addEventListener('click', () => {
-    verificaNumeri();
-});
 
-
+// Funzione per rigenerare i numeri randomici e svuotare i vari array e elementi html
 const generaNumeriHTML = document.getElementById('genera-numeri')
 
 function generaNumeri() {
@@ -87,6 +89,22 @@ function generaNumeri() {
     arrInputUtente = [];
     contatore = 0;
 }
+
+
+// Sezione per il richiamo delle funzioni
+createNumbers(); // Richiamo la funzione creazione numeri
+
+// Richiamo la funzione al click del bottone per pushare i numeri
+pushaInputHTML.addEventListener('click', () => {
+    pushaNumero();
+});
+
+// Richiamo la funzione al click del bottone per verificare i numeri
+verificaInputHTML.addEventListener('click', () => {
+    verificaNumeri();
+});
+
+// Richiamo la funzione al click del bottone per rigenerare i numeri
 generaNumeriHTML.addEventListener('click', () => {
     generaNumeri();
 });
